d1e69e638f834e4e5992c89860a54eb595b5011e
pmkelleher <pmkmobility@gmail.com>
2024-06-07 22:01:50 -0400

    Initial Commit

---
+++ hello-mdoyvr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hello-mdoyvr
+//
+//  Created by Patrick Kelleher on 6/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hello-mdoyvr/hello_mdoyvrApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_mdoyvrApp.swift
+//  hello-mdoyvr
+//
+//  Created by Patrick Kelleher on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct hello_mdoyvrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
